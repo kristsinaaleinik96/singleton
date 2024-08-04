bbd8d041dc3c9ff0775eaf28fb0ad83e12103ec8
Krystsina Aleinik <kristina.aleinik1996@gmail.com>
2024-08-04 19:00:13 +0200

    Create singleton

---
+++ DBConnection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Singleton
+{
+    internal class DBConnection
+    {
+       private static DBConnection? instance;
+       private SqlConnection connection; 
+       private DBConnection()
+        {
+            string connectionString = "Data Source=server;Initial Catalog=database;User ID=username;Password=password";
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            Console.WriteLine("The connection is established");
+        }
+
+        public static DBConnection GetInstance()
+        {
+            if (instance == null)
+            { 
+                instance = new DBConnection();
+            }
+            return DBConnection.instance;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DBConnection sqlConnection = DBConnection.GetInstance();
+        }
+    }
+}
